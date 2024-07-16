c518abc63df9b0b7cfbd5a09c56a600eae0d9f9b
WWiRRu <wiiiruriiax@gmail.com>
2024-07-16 11:09:45 +0200

    Update JS project, and added CodeWars README

---
+++ Projects/JavaScript/CodeWars/app.js
@@ -0,0 +1,89 @@
+/**
+ * @param {string} str
+ * @returns {number}
+ */
+function getCount(str) {
+    const vowels = ["a", "e", "i", "o", "u"]
+    return str.split("").filter(letter => vowels.includes(letter)).length;
+}
+/**
+ * @param {number[]} numbers
+ * @returns {number[]}
+ */
+function removeSmallest(numbers) {
+    const minNote = Math.min(...numbers);
+    const indexToRemove = numbers.findIndex(value => value === minNote);
+    const filteredNotes = [...numbers];
+    filteredNotes.splice(indexToRemove, 1);
+    return filteredNotes;
+}
+/**
+ * @param {string} s
+ * @returns {string}
+ */
+function getMiddle(s) {
+    let middle = Math.floor(s.length / 2);
+    if(s.length % 2 === 0){
+        return s[middle - 1] + s[middle];
+    } else {
+        return s[middle];
+    }
+}
+/**
+ * @returns {string}
+ */
+String.prototype.toJadenCase = function () {
+    return this.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
+};
+/**
+ * @param {string} str
+ * @returns {string}
+ */
+function disemvowel(str) {
+    return str.split('').filter(letter => !['a', 'e', 'i', 'o', 'u'].includes(letter.toLowerCase())).join('');
+}
+/**
+ * @param {number} num
+ * @returns {number}
+ */
+function squareDigits(num){
+    return Number(num.toString().split('').map((n) => Math.pow(Number(n), 2)).join(''));
+}
+/**
+ * @param {string} s
+ * @returns {number}
+ */
+function findShort(s){
+    return Math.min(...s.split(' ').map(word => word.length));
+}
+/**
+ * @param {Array} l
+ * @returns {number[]}
+ */
+function filter_list(l) {
+    return l.filter(function(v) {return typeof v == 'number'});
+}
+
+/**
+ * @param {string} cc
+ * @returns {string}
+ */
+function maskify(cc) {
+    return cc.slice(0, -4).replace(/./g, '#') + cc.slice(-4);
+}
+/**
+ * @param {Array.<Array.<number>>} data
+ * @returns {string[]}
+ */
+function openOrSenior(data){
+    return data.map(([age, handicap]) => age >= 55 && handicap > 7 ? 'Senior' : 'Open');
+}
+/**
+ * @param {number[]} integers
+ * @returns {number}
+ */
+function findOutlier(integers){
+    let even = integers.filter(n => n % 2 === 0);
+    let odd = integers.filter(n => n % 2 !== 0);
+    return even.length === 1 ? even[0] : odd[0];
+}
